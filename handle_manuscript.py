6789f85457f30c08baf026fd6556164b0cdcf497
Zhangben Wu <dupidog@gmail.com>
2021-12-05 00:05:10 +0800

    Add handle_manuscript.py

---
+++ handle_manuscript.py
@@ -0,0 +1,84 @@
+import glob
+import re
+import docx
+import math
+
+docx_list = glob.glob("*.docx")
+fo = open("output.csv", "w")
+fo.write("日期,题目,组织,文,图,文稿费,图稿费\n")
+
+for f in docx_list:
+    if re.search(r"^~\$.*\.docx", f):
+        continue
+    
+    # date
+    date_obj = re.search(r"^[0-9]{8}", f)
+    if date_obj:
+        date = date_obj.group()
+    else:
+        date = ""
+
+    # title
+    title_obj = re.search(r"）.*（", f)
+    if title_obj:
+        title = title_obj.group().strip("）（ ")
+    else:
+        title = ""
+
+
+    # get full text for getting further info
+    text = ""
+    file = docx.Document(f)
+    if file:
+        for para in file.paragraphs:
+            text += para.text + " "
+
+    # author
+    author_all_obj = re.search(r"文[、 /]图[/:：  ]{1,3}[^ \t\n\r]{2,4}[ \t\n\r]", text)
+    if not author_all_obj:
+        author_all_obj = re.search(r"图[、 /]文[/:：  ]{1,3}[^ \t\n\r]{2,4}[ \t\n\r]", text)
+    if author_all_obj:
+        author_all = author_all_obj.group().replace('/',' ').replace(':',' ').replace('：',' ').replace('\t',' ').replace('\r',' ').strip(" ").split(" ")[-1]
+        author_text = author_all
+        author_photo = author_all
+    else:
+        author_all = ""
+        author_text = ""
+        author_photo = ""
+            
+
+    if not author_all:
+        author_text_obj = re.search(r"文[/:： ]{1,3}[^ \t\n\r]{2,4}[ \t\n\r]", text)
+        if author_text_obj:
+            author_text = author_text_obj.group().replace('/',' ').replace(':',' ').replace('：',' ').replace('\t',' ').replace('\r',' ').strip(" ").split(" ")[-1]
+        else:
+            author_text = ""
+
+        author_photo_obj = re.search(r"图[/:： ]{1,3}[^ \t\n\r]{2,4}[ \t\n\r]", text)
+        if author_photo_obj:
+            author_photo = author_photo_obj.group().replace('/',' ').replace(':',' ').replace('：',' ').replace('\t',' ').replace('\r',' ').strip(" ").split(" ")[-1]
+        else:
+            author_photo = ""
+
+    #print("文/"+author_text+" 图/"+author_photo)
+
+    # charater count and fee
+    char_count = len(text)
+    if char_count < 300:
+        fee = 10
+    elif char_count < 500:
+        fee = 15
+    else:
+        fee = math.floor(char_count / 250) * 5 + 10
+
+    if fee > 100:
+        fee = 100
+
+    # photo count
+    photo_count = len(glob.glob(f.split(".")[0]+"*")) - 1
+    
+    # write csv
+    fo.write(date + "," + title + "," + title[0:2] + "," + author_text + "," + author_photo + "," + str(fee) + "," + str(photo_count*10) + "\n")
+
+fo.close()
+
